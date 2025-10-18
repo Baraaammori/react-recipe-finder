6726a9b2088a9a9f59541815cf479b500bfbf46a
Baraaammori <baraaammori2004@gmail.com>
2025-10-18 21:29:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="app">
       <h1>🍳 Recipe Finder</h1>
-      <p>Let's build this step by step and learn useEffect!</p>
       <RecipeFinder />
     </div>
   );
